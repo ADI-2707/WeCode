9372e38a19171bc6d3e159feafad03c305666934
ADI-2707 <aditya21singh2707@gmail.com>
2025-11-13 16:31:31 +0530

    stream api bug fixed

--- BackEnd/src/lib/stream.js
+++ BackEnd/src/lib/stream.js
@@ -2,7 +2,7 @@ import { StreamChat } from 'stream-chat'
 import { ENV } from './env.js'
 
 const apiKey = ENV.STREAM_API_KEY
-const apiSecret = ENV.STREAM_SECRET_KEY
+const apiSecret = ENV.STREAM_API_SECRET
 
 if(!apiKey || !apiSecret) {
     console.error('STREAM_API_KEY or STREAM_SECRET_KEY is missing')
